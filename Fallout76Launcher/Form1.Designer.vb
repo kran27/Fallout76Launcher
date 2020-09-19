b452ba93e5513762ecb1511c487cb42acae1530a
Kranberri <56488228+kran27@users.noreply.github.com>
2020-09-19 02:31:44 -0700

    Initial Commit
    At this point, the launcher works to launch the Steam version of the game, and the Support button opens this GitHub page.

---
+++ Fallout76Launcher/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.Fallout76Launcher.My.Resources.Resources.play
+        Me.PictureBox1.Location = New System.Drawing.Point(743, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(63, 26)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.Fallout76Launcher.My.Resources.Resources.options
+        Me.PictureBox2.Location = New System.Drawing.Point(724, 85)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(82, 24)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = Global.Fallout76Launcher.My.Resources.Resources.support
+        Me.PictureBox3.Location = New System.Drawing.Point(721, 137)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(85, 24)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackgroundImage = Global.Fallout76Launcher.My.Resources.Resources._exit
+        Me.PictureBox4.Location = New System.Drawing.Point(767, 189)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(39, 23)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowText
+        Me.BackgroundImage = Global.Fallout76Launcher.My.Resources.Resources.bg
+        Me.ClientSize = New System.Drawing.Size(835, 400)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fallout 76"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ Fallout76Launcher/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+#Region " Move Form "
+
+    ' [ Move Form ]
+    '
+    ' // By Elektro 
+
+    Public MoveForm As Boolean
+    Public MoveForm_MousePosition As Point
+
+    Public Sub MoveForm_MouseDown(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseDown ' Add more handles here (Example: PictureBox1.MouseDown)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = True
+            MoveForm_MousePosition = e.Location
+        End If
+
+    End Sub
+
+    Public Sub MoveForm_MouseMove(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseMove ' Add more handles here (Example: PictureBox1.MouseMove)
+
+        If MoveForm Then
+            Me.Location = Me.Location + (e.Location - MoveForm_MousePosition)
+        End If
+
+    End Sub
+
+    Public Sub MoveForm_MouseUp(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseUp ' Add more handles here (Example: PictureBox1.MouseUp)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = False
+        End If
+
+    End Sub
+#End Region
+
+#Region " Button Hover "
+    Private Sub PlayButton_MouseHover(sender As Object, e As EventArgs) Handles PictureBox1.MouseHover
+        PictureBox1.Image = My.Resources.playlit
+    End Sub
+
+    Private Sub PlayButton_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox1.MouseLeave
+        PictureBox1.Image = My.Resources.play
+    End Sub
+    Private Sub OptionsButton_MouseHover(sender As Object, e As EventArgs) Handles PictureBox2.MouseHover
+        PictureBox2.Image = My.Resources.optionslit
+    End Sub
+
+    Private Sub OptionsButton_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox2.MouseLeave
+        PictureBox2.Image = My.Resources.options
+    End Sub
+    Private Sub SupportButton_MouseHover(sender As Object, e As EventArgs) Handles PictureBox3.MouseHover
+        PictureBox3.Image = My.Resources.supportlit
+    End Sub
+
+    Private Sub SupportButton_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox3.MouseLeave
+        PictureBox3.Image = My.Resources.support
+    End Sub
+    Private Sub ExitButton_MouseHover(sender As Object, e As EventArgs) Handles PictureBox4.MouseHover
+        PictureBox4.Image = My.Resources.exitlit
+    End Sub
+
+    Private Sub ExitButton_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox4.MouseLeave
+        PictureBox4.Image = My.Resources._exit
+    End Sub
+
+#End Region
+
+#Region " Button Clicks "
+    Private Sub Play_Click(ByVal sender As System.Object,
+                            ByVal e As System.EventArgs) Handles PictureBox1.Click
+        Dim clickedLabel = TryCast(sender, Label)
+        System.Diagnostics.Process.Start("steam://rungameid/1151340")
+        Application.Exit()
+    End Sub
+    Private Sub Options_Click(ByVal sender As System.Object,
+                        ByVal e As System.EventArgs) Handles PictureBox2.Click
+        Dim clickedLabel = TryCast(sender, Label)
+        MessageBox.Show("This Feature is not yet implemented :)", "Whoops!", MessageBoxButtons.OK)
+    End Sub
+    Private Sub Support_Click(ByVal sender As System.Object,
+                        ByVal e As System.EventArgs) Handles PictureBox3.Click
+        Dim clickedLabel = TryCast(sender, Label)
+        System.Diagnostics.Process.Start("http://github.com/kran27/Fallout76Launcher")
+    End Sub
+    Private Sub Exit_Click(ByVal sender As System.Object,
+                        ByVal e As System.EventArgs) Handles PictureBox4.Click
+        Dim clickedLabel = TryCast(sender, Label)
+        Application.Exit()
+    End Sub
+#End Region
+End Class
